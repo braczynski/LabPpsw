ca23921a58c3dfb37f6fedf98ce499435ec33c4d
unknown <rakuuu200@gmail.com>
2025-03-19 14:43:08 +0100

    Przycisk

--- Current/main.c
+++ Current/main.c
@@ -6,6 +6,8 @@
 #define LED2_bm 0x40000
 #define LED3_bm 0x80000
 
+#define S0_bm 0X10
+
 void delay(unsigned int uiTimeMs);
 void LedInit(void);
 void LedOn(unsigned char ucLedIndeks);
@@ -13,6 +15,7 @@ void LedOn(unsigned char ucLedIndeks);
 void delay(unsigned int uiTimeMs)
 {
 	unsigned int uiCounter;
+	
 	for(uiCounter = 0; uiCounter < (MIL*uiTimeMs); uiCounter++)
 	{
 	}
@@ -22,8 +25,17 @@ void LedInit(){
 	
 	IO1DIR |= LED0_bm + LED1_bm + LED2_bm + LED3_bm;
 	
-	IO1SET |= LED0_bm;
+}
+
+enum ButtonState {RELASED, PRESSED};
+
+enum ButtonState ReadKeyboard1(){                                                      
 	
+	if((IO0PIN &= S0_bm) == S0_bm){
+		return PRESSED;
+	} else{
+		return RELASED;
+	}
 }
 
 void LedOn(unsigned char ucLedIndeks){
@@ -54,13 +66,15 @@ int main()
 	
 	while(1)
 	{
-		LedOn(0);
-		delay(250);
-		LedOn(1);
-		delay(250);
-		LedOn(2);
-		delay(250);
-		LedOn(3);
-		delay(250);
+	
+		if(ReadKeyboard1() == 1)
+		{
+			
+			LedOn(1);
+		} else{
+			
+			LedOn(0);
+		}
+		
 	}
 }
