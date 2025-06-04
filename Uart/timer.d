a00010a2159e8ed892e2294445e91e0188aedf44
Bartłomiej Raczyński <53814364+braczynski@users.noreply.github.com>
2025-06-04 14:08:08 +0200

    Delete Uart directory
    clean

--- Uart/timer.d
+++
@@ -1,3 +0,0 @@
-.\timer.o: ..\Library\timer.c
-.\timer.o: C:\Keil\ARM\Inc\Philips\LPC21xx.H
-.\timer.o: ..\Library\timer.h
--- Uart/uart.h
+++
@@ -1,6 +0,0 @@
-#ifndef UART_H
-#define UART_H
-
-void UART_InitWithInt(unsigned int uiBaudRate);
-
-#endif
